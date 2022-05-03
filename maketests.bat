f4afdb120226f5a63dd3d7ab6f90d94ddfedbb06
LooseEthics <36419655+LooseEthics@users.noreply.github.com>
2022-05-03 15:52:28 +0200

    f

---
+++ maketests.bat
@@ -0,0 +1,6 @@
+php parse.php <test/echo.txt >test/echo.xml
+php parse.php <test/fibonacci.txt >test/fibonacci.xml
+php parse.php <test/helloworld.txt >test/helloworld.xml
+php parse.php <test/lfsr.txt >test/lfsr.xml
+php parse.php <test/str_inv.txt >test/str_inv.xml
+php parse.php <test/turing.txt >test/turing.xml
